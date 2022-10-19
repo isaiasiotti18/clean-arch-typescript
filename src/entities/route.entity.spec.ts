f273b745589a3abfa6e39a22c331747724c25456
Isaias Batista dos Santos <isaiasiottiprofissional@gmail.com>
2022-10-19 20:04:51 -0300

    Tests

--- src/entities/route.entity.spec.ts
+++ src/entities/route.entity.spec.ts
@@ -1,42 +1,51 @@
-import Route, { RouteProps } from './route.entity';
-describe('Route Tests', () => {
-  test('constructor', () => {
-    let routeProps: RouteProps = {
-      title: "My Route",
-      startPosition: { lat: 0, lng: 2 },
-      endPosition: { lat: 2, lng: 4 }
-    }
-    let route = new Route(routeProps)
-    expect(route.props).toStrictEqual({
-      ...routeProps, points: []
-    })
+import { LatLng, Route, RouteProps } from "./route.entity";
 
+describe("Route Tests", () => {
+
+  let routeProps: RouteProps
+  let route: Route
+
+  beforeAll(() => {
     routeProps = {
-      title: "My Route",
-      startPosition: { lat: 0, lng: 2 },
-      endPosition: { lat: 2, lng: 4 },
-      points: [
-        { lat: 0, lng: 2 },
-        { lat: 2, lng: 4 }
-      ]
-    }
-    route = new Route(routeProps)
-    expect(route.props).toStrictEqual({
-      ...routeProps, points: [
-        { lat: 0, lng: 2 },
-        { lat: 2, lng: 4 }
-      ]
-    })
+      title: "minha rota",
+      startPosition: { lat: 0, lng: 1 },
+      endPosition: { lat: 2, lng: 3 },
+    };
+
+    route = new Route(routeProps);
   })
 
-  test('update title method', () => {
-    const routeProps: RouteProps = {
-      title: "My Route",
-      startPosition: { lat: 0, lng: 2 },
-      endPosition: { lat: 2, lng: 4 }
-    }
-    const route = new Route(routeProps)
-    route.updateTitle('Route')
-    expect(route.title).toBe('Route')
-  })
-})
+  test("constructor", () => {
+    expect(route.props).toStrictEqual({
+      ...routeProps,
+      points: [],
+    });
+    expect(route.id).toBeDefined();
+    expect(route.props).toStrictEqual({
+      ...routeProps,
+      points: [{ lat: 10, lng: 11 }],
+    });
+  });
+
+  test("updateTitle method", () => {
+    route.updateTitle("title updated");
+    expect(route.title).toBe("title updated");
+  });
+
+  test("updatePosition method", () => {
+    const startPosition: LatLng = { lat: 10, lng: 20 };
+    const endPosition: LatLng = { lat: 30, lng: 40 };
+    route.updatePosition(startPosition, endPosition);
+    expect(route.startPosition).toBe(startPosition);
+    expect(route.endPosition).toBe(endPosition);
+  });
+
+  test("updatePoints method", () => {
+    const points: LatLng[] = [
+        { lat: 10, lng: 20 }
+    ];
+    route.updatePoints(points);
+    expect(route.points).toHaveLength(1);
+    expect(route.points).toStrictEqual(points)
+  });
+});
